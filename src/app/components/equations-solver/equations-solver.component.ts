bcd12866e24c5b3ef2ea8fe5319f3627855ca5e6
TarekSaeed0 <naminode2020@gmail.com>
2025-11-15 14:55:30 +0200

    refactor: minor changes

--- src/app/components/equations-solver/equations-solver.component.ts
+++ src/app/components/equations-solver/equations-solver.component.ts
@@ -67,6 +67,7 @@ export class EquationsSolverComponent {
       setTimeout(() => {
         this.parameters()?.nativeElement.scrollIntoView({
           behavior: "smooth",
+					block: "center",
         });
       });
     });
